ba8fdd995697ba1b2b2001efaad209d698211c75
Lisandro <lisandro_luna@ymail.com>
2020-08-20 22:20:36 -0300

    Se agregan detalles.

--- Clase 02/Practica 02/js/clase02.js
+++ Clase 02/Practica 02/js/clase02.js
@@ -1,5 +1,7 @@
 reserva = parseInt(prompt('Ingrese su numero de reserva: '))
-
+console.log('Se registra numero de reserva: ' + reserva)
 if ((reserva > 10) && (reserva < 50)){
     alert('Su reserva esta disponible.')
+} else {
+    alert('Su reserva ya fue utilizada.')
 }
