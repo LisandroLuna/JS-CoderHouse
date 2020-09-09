2e45cbfdad0799714e77504a7ff58db6f1a43ee8
Lisandro <lisandro_luna@ymail.com>
2020-09-08 22:11:01 -0300

    Clase 05.

---
+++ Clase 05/Practica 05/js/scripts.js
@@ -0,0 +1,13 @@
+function MyCar(marca, modelo, anio){
+    this.marca = marca
+    this.modelo = modelo
+    this.anio = anio
+}
+
+const car = new MyCar("Ford", "Taunus",1999)
+
+const miAuto ={
+    marca: "Fiat",
+    color: "Rojo",
+    anio: 1999
+}
--- Proyecto/js/scripts.js
+++ Proyecto/js/scripts.js
@@ -1,28 +1,28 @@
 function calc(){
-    var sect = parseInt(prompt(
+    let sect = parseInt(prompt(
                         'Ingrese el numero de secciones para su sitio excluyendo blog y tienda si las requiere:\n' +
                     'Ej.: si su sitio tiene una pagina de inicio, seccion galeria y seccion de contacto serian 3 secciones.'
             ))
-    var opt = parseInt(prompt(
+    let opt = parseInt(prompt(
             'Ingrese la opcion que desea:' +
                     '\n1- Sitio Simple' +
                     '\n2- Sitio + Blog' +
                     '\n3- Sitio + Ecommerce' +
                     '\n4- Sitio + Blog + Ecommerce'
             ))
-    total = calcTotal(sect, opt)
+    let total = calcTotal(sect, opt)
     alert("Costo total de su sitio web: $" + total)
 }
 
 function calcSect(sect){
-    var sectPrice = 500
+    let sectPrice = 500
     return sectPrice*sect
 }
 
 function calcTotal(sect, opt){
-    var price = [calcSect(sect),3000, 3000, 3000, 3000]
-    var total = 0
-    for(var i = 0; i<=opt; i++){
+    let price = [calcSect(sect),3000, 3000, 3000, 3000]
+    let total = 0
+    for(let i = 0; i<=opt; i++){
         total = total + price[i]
     }
     return total
