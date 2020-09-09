78c77b40542af47f276b6b0bc57e2357d01993e4
Lisandro <lisandro_luna@ymail.com>
2020-09-09 10:56:14 -0300

    Se agregan objetos.

--- Proyecto/js/scripts.js
+++ Proyecto/js/scripts.js
@@ -1,29 +1,80 @@
-function calc(){
-    let sect = parseInt(prompt(
-                        'Ingrese el numero de secciones para su sitio excluyendo blog y tienda si las requiere:\n' +
-                    'Ej.: si su sitio tiene una pagina de inicio, seccion galeria y seccion de contacto serian 3 secciones.'
-            ))
-    let opt = parseInt(prompt(
-            'Ingrese la opcion que desea:' +
-                    '\n1- Sitio Simple' +
-                    '\n2- Sitio + Blog' +
-                    '\n3- Sitio + Ecommerce' +
-                    '\n4- Sitio + Blog + Ecommerce'
-            ))
-    let total = calcTotal(sect, opt)
-    alert("Costo total de su sitio web: $" + total)
-}
+const basePrice = 1500;
+const sectPrice = 500;
+const blogPrice = 3000;
+const ecomPrice = 3000;
 
-function calcSect(sect){
-    let sectPrice = 500
-    return sectPrice*sect
-}
-
-function calcTotal(sect, opt){
-    let price = [calcSect(sect),3000, 3000, 3000, 3000]
-    let total = 0
-    for(let i = 0; i<=opt; i++){
-        total = total + price[i]
+function Web(sect, blog, ecom) {
+    this.sect = sect;
+    this.blog = blog;
+    this.ecom = ecom;
+    this.price = function (){
+        let price = basePrice;
+        if(this.calcSect()>0) {
+            price += this.calcSect()
+        }
+        if(this.blog === true){
+            price += blogPrice
+        }
+        if(this.ecom === true){
+            price += ecomPrice
+        }
+        return price
+    };
+    this.calcSect = function () {
+        return sectPrice*this.sect
     }
-    return total
 }
+
+function calc(){
+    let sect = 0;
+    let blog, ecom, stop = false;
+    while(stop === false) {
+        sect = parseInt(prompt(
+            'Ingrese el numero de secciones para su sitio excluyendo blog y tienda si las requiere:\n' +
+            'Ej.: si su sitio tiene una pagina de inicio, seccion galeria y seccion de contacto serian 3 secciones.'
+        ));
+        if(sect<1 || !Number.isInteger(sect)){
+            alert('Ingrese un numero mayor a 1!');
+            console.log('Error en seleccion de numero de secciones.');
+            continue;
+        }
+        console.log('Secciones: ' + sect);
+        let opt = parseInt(prompt(
+            'Ingrese la opcion que desea:' +
+            '\n1- Sitio Simple' +
+            '\n2- Sitio + Blog' +
+            '\n3- Sitio + Ecommerce' +
+            '\n4- Sitio + Blog + Ecommerce'
+        ));
+        switch (opt) {
+            case 1:
+                stop = true;
+                break;
+            case 2:
+                blog = true;
+                stop = true;
+                console.log('Modelo: ' + sect + ' + Blog');
+                break;
+            case 3:
+                ecom = true;
+                stop = true;
+                console.log('Modelo: ' + sect + ' + Tienda');
+                break;
+            case 4:
+                blog = true;
+                ecom = true;
+                stop = true;
+                console.log('Modelo: ' + sect + ' + Blog + Tienda');
+                break;
+            default:
+                alert('Ingrese una opcion valida!');
+                console.log('Error de seleccion de modelo de sitio.')
+
+        }
+    }
+    const webCalc = new Web(sect, blog, ecom);
+    console.log(webCalc);
+    alert("Costo total de su sitio web: $" + webCalc.price());
+    console.log("Costo total: $" + webCalc.price())
+}
+
